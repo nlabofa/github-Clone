883c1802d1b0972759265f46fd2f589367314e02
Opeyemi Agbeja <niyola59@gmail.com>
2016-09-05 19:03:51 +0100

    initial initialisation. It also contains the overall project folder

---
+++ app/services/github.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GithubService {
+    private username='bradtraversy';
+    private client_id = '9b874289c9b9dde99f5b';
+    private client_secret='2987b03e6d96ec909168beca9be0bf0e16e737e0';
+    
+    constructor(private _http: Http){
+        console.log('Github Service Started...');
+    }
+    
+    getUser(){
+        return this._http.get('https://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret='+this.client_secret)
+            .map(res => res.json());
+    }
+    
+    getRepos(){
+        return this._http.get('https://api.github.com/users/'+this.username+'/repos?client_id='+this.client_id+'&client_secret='+this.client_secret)
+            .map(res => res.json());
+    }
+    
+    updateUsername(username:any){
+        this.username = username;
+    }
+}
